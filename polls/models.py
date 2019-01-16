00bd6c8b97b1e8699dd3c44fe5de16e77c50fb0f
Chaeoh <limchyo>
2019-01-16 11:49:54 +0900

    updated

--- polls/models.py
+++ polls/models.py
@@ -2,16 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    # 질문 데이터
     question_text = models.CharField(max_length=200)
+    # 발행일(날짜/시각)
     pub_date = models.DateTimeField('date published')
 
+    # 질문 내용 표기
     def __str__(self):
         return self.question_text
 
+# 선택지
 class Choice(models.Model):
+    # 질문 가져오기
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # 선택지 데이터
     choice_text = models.CharField(max_length=200)
+    # 투표수(기본값 0)
     votes = models.IntegerField(default=0)
 
+    # 선택지 내용 표기
     def __str__(self):
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,10 @@ from .models import Question, Choice
 
 # Create your views here.
 def index(request):
+    # 저장된 질문 모두 가져온다
+    # 내림차순으로 정렬 (최근 5개)
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    # 모든 객체 호출, 내림차순 정렬, 그 중 최근 객체 5개
     ctx = { "latest_question_list" : latest_question_list }
     return render(request, "index.html", ctx)
 
@@ -17,15 +20,26 @@ def detail(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
+        # Question와 연결된 choice 객체와 연결
+        # question.choice_set은 질문 속 선택지 객체를 말한다
+        # choice.get()은 선택 받은 선택지이다
+        # 따라서 폼을 통해 선택 받은 선택지를 인스턴스로 지정
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        # request.POST.get("choice")
+
+    # 선택지 미선택 제출시 예외 처리
     except (KeyError, Choice.DoesNotExist):
         return render(request, "detail.html", {
             "question" : question,
             "error_message" : "You didn't select a choice.",
         })
+
+    # 선택하고 제출한 경우
     else:
+        # 투표수 데이터에 더하기 1을 하고 저장
         selected_choice.votes += 1
         selected_choice.save()
+        # 해당 질문에 대한 결과페이지로 리다이렉트
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 def results(request, question_id):
